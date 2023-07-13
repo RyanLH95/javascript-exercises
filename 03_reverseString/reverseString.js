d4becff2d880941769264c92085db0072afad428
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-07-13 11:12:15 +0100

    create and test function that reverses string. e.g. 'hello world' to 'olleh dlro'

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let splitString = str.split("");
+    console.log(splitString)
 
+    let reverseArray = splitString.reverse();
+    console.log(reverseArray);
+
+    let joinArray = reverseArray.join("");
+
+    return joinArray;
 };
+
+console.log(reverseString("hello"))
 
 // Do not edit below this line
 module.exports = reverseString;

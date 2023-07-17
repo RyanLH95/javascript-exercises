fc63296a771720090f37f2aadbe5666c1aeeb422
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-07-17 13:19:32 +0100

    Add '06_leapYears/leapYears.js'

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,7 @@
 const leapYears = function(year) {
 
   return ((year % 4 === 0) && (year % 100 != 0) || (year % 400 == 0)) 
-  // "year variable modified by 4 is equal to 0 (strict equality operator" && 
+  // "year variable modified by 4 is equal to 0 (strict equality operator)" && 
   // "year variable modified by 100 and converted by an !=(inequality operator to convert)" ||(or)
   // "year variable modified by 400 and converted by an !=(equality operator to convert)"
 };

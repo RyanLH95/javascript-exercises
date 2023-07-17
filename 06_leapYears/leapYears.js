ef0f2f35d41fbd12a10233b024814a4546541c3a
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-07-17 13:01:09 +0100

    add '06_leapYears/leapYears.js' and '06_leapYears/leapYears.spec.js'

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+  return ((year % 4 === 0) && (year % 100 != 0) || (year % 400 == 0)) 
+  // "year variable modified by 4 is equal to 0" && 
+  // "year variable modified by 100 and converted by an !=(inequality operator)" ||(or)
+  // "year variable modified by 400 and converted by an !=(inequality operator)"
 };
+
+console.log(leapYears())
+  
 
 // Do not edit below this line
 module.exports = leapYears;

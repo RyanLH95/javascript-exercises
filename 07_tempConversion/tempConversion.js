de858d06b6f9e9391d684b9f1d25facca7992f7e
Ryan Henry <126239094+RyanLH95@users.noreply.github.com>
2023-07-18 14:43:36 +0100

    Add '07_tempConversion/tempConversion.js' and '07_tempConversion/tempConversion.spec.js'

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius())
+
+const convertToFahrenheit = function(celsius) {
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
+
+console.log(convertToFahrenheit())
 
 // Do not edit below this line
 module.exports = {
